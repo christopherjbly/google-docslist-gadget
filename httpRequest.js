4ac5cc2d226572ca1d2914c443d69f3aabfb1b9e
saturn5 <saturn5@810aaa7a-5255-0410-b6e2-2be4e376e1e0>
2008-09-18 10:40:01 +0000

    functional login and logout, error messages, http requests, new doc creation

---
+++ httpRequest.js
@@ -0,0 +1,165 @@
+// Copyright 2007 Google Inc.
+// All Rights Reserved.
+
+// @fileoverview Functions for server communications through XMLHttpRequest
+
+function createXhr() {
+  var xhr;
+
+  try {
+    xhr = framework.google.betaXmlHttpRequest();
+  } catch (e) {
+    xhr = new XMLHttpRequest();
+  }
+
+  return xhr;
+}
+
+var httpRequest = new HTTPRequest();
+
+
+
+function HTTPRequest() {
+  this.packet = new createXhr();
+  this.handler = null;
+  this.failedHandler = null;
+  // Token for timeout timer.
+  this.timeoutTimer = null;
+}
+
+
+
+HTTPRequest.available = true;
+HTTPRequest.queue = [];
+
+
+/**
+ * Function used to allow a time between httpRequests, so as not to clutter
+ * the servers. This function is static since all requests in the gadget are
+ * made to the same server and thus must handle all objects of the class type
+ */
+HTTPRequest.finishedGracePeriod = function() {
+  HTTPRequest.available = true;
+  if (HTTPRequest.queue.length > 0) {
+    var request = HTTPRequest.queue.shift();
+    request.requestObject.connect(request.data, request.handler, request.failedHandler);
+  }
+};
+
+
+
+/**
+ * Sends out a request using XMLHttpRequest
+ * @param {String} data The data to be packed
+ */
+HTTPRequest.prototype.connect = function (data, handler, failedHandler) {
+  
+  if (!HTTPRequest.available) {
+    // The server fails to handle too many requests at a time so we need to
+    // queue them.
+    if (HTTPRequest.queue.length > 0 && data.indexOf(URL.OFFSET_PARAM) > -1) {
+      // We check to see if the user requested the next/previews page twice
+      // this is the only case we need to check as we check it before adding
+      // every request so if 3 or more consecutive calls are made only the
+      // latest is kept. There is no need for further checking, as other types
+      // of request are more naturally stoped from queueing elsewhere.
+      if (HTTPRequest.queue[HTTPRequest.queue.length - 1].data.indexOf(
+          URL.OFFSET_PARAM) > -1) {
+        // if this happens, we only keep the latest request arround
+        HTTPRequest.queue.pop();
+      }
+    }
+    HTTPRequest.queue.push({ requestObject: this, data: data, handler: handler, failedHandler: failedHandler} );
+    return;
+  }
+  try {
+    loading.visible = true;
+  } catch(e) {
+    // rare occurance, if the details view is just closing
+    return;
+  }
+
+  // Check if network is online.
+  if (!framework.system.network.online) {
+    this.onFailure();
+    return;
+  }
+
+  this.handler = handler;
+  this.failedHandler = failedHandler;
+  this.packet.abort();
+  this.packet.onreadystatechange = this.receivedData.bind(this);
+  this.packet.open('POST', this.url, true);
+  this.packet.setRequestHeader('cookie', 'none');
+  this.packet.setRequestHeader('X-Wap-Proxy-Cookie', 'none');
+  this.packet.setRequestHeader('Cache-Control', 'no-cache, no-transform');
+  this.packet.setRequestHeader('Content-Type',
+                               'application/x-www-form-urlencoded');
+  // Impersonate J2ME midlets, which send this user agent
+  // for untrusted (unsigned) midlets.
+  // This ensures MILU properly serves mobile-friendly content.
+  this.packet.setRequestHeader('User-agent', 'UNTRUSTED/1.0');
+  this.packet.setRequestHeader('Host', this.host);
+  this.packet.send(data);
+
+  // Set timeout timer.
+  this.clearTimeout();  
+  this.timeoutTimer = view.setTimeout(this.onTimeout.bind(this), CONNECTION.TIMEOUT);
+
+  HTTPRequest.available = false;
+  debug.trace('Request sent to server : ' + data);
+};
+
+HTTPRequest.prototype.clearTimeout = function() {
+  if (this.timeoutTimer) {
+    view.clearTimeout(this.timeoutTimer);
+    this.timeoutTimer = null;
+  }
+};
+
+HTTPRequest.prototype.onTimeout = function() {
+  debug.error('Request timed out.');
+  this.packet.abort();  
+  setTimeout(HTTPRequest.finishedGracePeriod, CONNECTION.TIME_BETWEEN_REQUESTS);
+  this.hideLoading();
+  this.onFailure();
+};
+
+HTTPRequest.prototype.hideLoading = function() {
+  try {
+    loading.visible = false;
+  } catch(e) {
+    // rare occurance, if the details view is closing when receiving data
+    debug.warning('Could not hide loading image.');
+  }  
+};
+
+HTTPRequest.prototype.onFailure = function() {
+  if (this.failedHandler !== null) {
+    var status = this.packet.readyState == 4 ? this.packet.status : 0;
+    this.failedHandler(status, this.packet.responseText);
+  } else {
+    errorMessage.display(SERVER_OR_NETWORK_ERROR);
+  }
+};
+
+HTTPRequest.prototype.receivedData = function() {
+  if (!this.packet) {
+    return;
+  }
+  if (this.packet.readyState != 4) {
+    return;
+  }
+  debug.trace('All data received here...');
+  this.hideLoading();  
+  this.clearTimeout();  
+  setTimeout(HTTPRequest.finishedGracePeriod, CONNECTION.TIME_BETWEEN_REQUESTS);
+  if (this.packet.status != 200) {
+    debug.error('A transfer error has occured !');
+    this.onFailure();
+    return;
+  }
+  if (this.handler !== null) {
+    this.handler(this.packet.responseText);
+  }
+};
---
+++ login.js
@@ -0,0 +1,185 @@
+// Copyright 2007 Google Inc.
+// All Rights Reserved.
+
+// @fileoverview Functions for handling the login view and session of the gadget
+
+function LoginSession() {
+  
+  login.onclick = this.login.bind(this);
+  commandSignout.onclick = this.logout.bind(this);  
+  
+  this.setup();
+    
+  if (user.value !== '' && pass.value !== '') {
+    remember.value = true;
+    this.login();
+  }      
+}
+
+/**
+ * Setup login field defaults
+ */
+LoginSession.prototype.setup = function() {
+  this.token = '';  
+  username.innerText = '';
+  username.visible = false;  
+  user.value = options.getValue('user');
+  pass.value = options.getValue('pass');
+};
+
+/**
+ * Login the user either at startup or from the login view
+ */
+LoginSession.prototype.login = function() {
+  if (loading.visible) return;
+  
+  if (remember.value) {
+    options.putValue('user', user.value);
+    options.encryptValue('user');
+    options.putValue('pass', pass.value);
+    options.encryptValue('pass');
+  } else {
+    options.putValue('user', '');
+    options.putValue('pass', '');
+  }
+  
+  var userValue = (user.value.indexOf('@') == -1) ? user.value + '@' + CONNECTION.DEFAULT_DOMAIN : user.value;
+  var passValue = pass.value;
+  
+  var domain = userValue.indexOf('@' + CONNECTION.DEFAULT_DOMAIN);
+  this.username = domain == -1 ? userValue : userValue.substr(0, domain);
+
+  var data = {  'Email': userValue, 
+                'Passwd': passValue, 
+                'service': CONNECTION.AUTH_SERVICE, 
+                'source': REPORTED_CLIENT_NAME,
+                'accountType': CONNECTION.AUTH_TYPE
+              }.toQueryString();
+
+  httpRequest.host = CONNECTION.AUTH_HOST;
+  httpRequest.url = CONNECTION.AUTH_URL;  
+  httpRequest.connect(data, this.loginSuccess.bind(this), this.loginError.bind(this));    
+};
+
+/**
+ * Save auth token
+ */
+LoginSession.prototype.loginSuccess = function(responseText) {
+
+  this.token = this.getCookie('Auth', responseText);
+  
+  if (this.token) {
+    username.innerText = this.username;
+    username.visible = true;
+    doclist.login();
+  } else {
+    this.loginError();
+  }
+};
+
+/**
+ * Display login error
+ */
+LoginSession.prototype.loginError = function(status, responseText) {
+
+  if (status == 403) {
+    var error = this.getCookie('Error', responseText) || 'Unknown';
+  }
+  
+  errorMessage.display(LOGIN_ERRORS[error] || SERVER_OR_NETWORK_ERROR);
+};
+
+/**
+ * Log the user out
+ */
+LoginSession.prototype.logout = function() {
+  this.setup();
+  doclist.logout();
+};
+
+/**
+ * Match and return cookie
+ */
+LoginSession.prototype.getCookie = function(key, responseText) {
+  var matches = responseText.match(new RegExp(key+'\=(.*)'));
+  return (matches && matches.length > 1) ? matches[1] : '';
+};
+
+/**
+ * This function is called onkeypress of the username editbox. accepts tab and
+ * enter key.
+ */
+LoginSession.prototype.onUsernameKeyPress = function() {
+  if (event.keycode == KEYS.ENTER) {
+    if (pass.value === '') {
+      pass.focus();
+    } else {
+      this.login();
+    }
+    event.returnValue = false;
+  }
+};
+
+
+
+/**
+ * This function is called onkeydown of the password editbox. accepts enter key.
+ */
+LoginSession.prototype.onPasswordKeyPress = function() {
+  if (event.keycode == KEYS.ENTER) {
+    if (user.value.length === 0) {
+      user.focus(); // Put focus on user field.
+    } else {
+      this.login();
+    }
+    event.returnValue = false;
+  }
+};
+
+
+
+/**
+ * This function is called onkeydown of the remember checkbox.
+ */
+LoginSession.prototype.onRememberKeyPress = function() {
+  if (event.keycode == KEYS.ENTER ||
+      event.keycode == KEYS.SPACE) {
+    remember.value = !remember.value;
+    this.onRememberFocus(true);
+  }
+};
+
+
+
+LoginSession.prototype.onLoginKeyPress = function() {
+  if (event.keycode == KEYS.ENTER ||
+      event.keycode == KEYS.SPACE) {
+    // Put focus on user field if its empty.
+    if (user.value.length === 0) {
+      user.focus();
+    } else {
+      this.login();
+    }
+  }
+};
+
+
+
+/*
+ * This function is called when the remember checkbox gets or looses focus
+ */
+LoginSession.prototype.onRememberFocus = function(got) {
+  rememberFocus.visible = got;
+};
+
+
+
+/*
+ * This function is called when the action combo gets or looses focus
+ */
+LoginSession.prototype.onLoginFocus = function(got) {
+  login.image = got ? 'images/action_hover.png' : 'images/action_default.png';
+};
+
+// instantiate object in the global scope
+var loginSession = new LoginSession();
