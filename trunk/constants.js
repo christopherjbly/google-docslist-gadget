5f01f693c194544fa93fce9d7b45238b507b0a91
james.yum <james.yum@810aaa7a-5255-0410-b6e2-2be4e376e1e0>
2008-09-25 04:55:46 +0000

    code cleanup.

--- trunk/constants.js
+++ trunk/constants.js
@@ -1,14 +1,16 @@
 // Copyright 2008 Google Inc.
 // All Rights Reserved.
 
-// @fileoverview Constants used throughout the plugin
+// @fileoverview Constants used throughout the gadget.
 
-var REPORTED_CLIENT_NAME = 'gd-docslist-gadget-' + VERSION_STRING;
+var REPORTED_CLIENT_NAME = 'gd-docslist-gadget-' + strings.VERSION_STRING;
+
+// TODO: move these to files.
 
 var CONNECTION = {
-  TIME_BETWEEN_REQUESTS: 1000, // 1 second
-  TIMEOUT: 30000, // 30 seconds
-  REFRESH_INTERVAL: 60000, // 60 seconds
+  TIME_BETWEEN_REQUESTS: 1000,  // 1 second
+  TIMEOUT: 30000,  // 30 seconds
+  REFRESH_INTERVAL: 5 * 60 * 1000,  // 60 seconds
 
   DEFAULT_DOMAIN: 'gmail.com',
   AUTH_HOST: 'www.google.com',
@@ -20,23 +22,20 @@ var CONNECTION = {
   MAX_RESULTS_PARAM: 'max-results',
   MAX_RESULTS: 25,
   SEARCH_PARAM: 'q',
-  START_INDEX_PARAM: 'start-index'
-};
+  START_INDEX_PARAM: 'start-index' };
 
 var UI = {
   MIN_WIDTH: 170,
   MIN_HEIGHT: 200,
   MIN_DATE_WIDTH: 85,
   MAX_AUTOFILL: 5,
-  ERROR_MESSAGE_TIMEOUT: 3000
-};
+  ERROR_MESSAGE_TIMEOUT: 3000 };
 
 var UPLOAD_STATUS = {
   WAITING: false,
   LOADING: 'loading',
   SUCCESS: 'success',
-  ERROR: 'error'
-};
+  ERROR: 'error' };
 
 var FILE_TYPES = {
   'doc': 'document',
@@ -53,8 +52,7 @@ var FILE_TYPES = {
   'prc': 'presentation',
   'htm': 'document',
   'html': 'document',
-  'rtf': 'document'
-};
+  'rtf': 'document' };
 
 var MIME_TYPES = {
   'csv': 'text/csv',
@@ -71,15 +69,13 @@ var MIME_TYPES = {
   'tab': 'text/tab-separated-values',
   'txt': 'text/plain',
   'tsv': 'text/tab-separated-values',
-  'xls': 'application/vnd.ms-excel'
-};
+  'xls': 'application/vnd.ms-excel' };
 
 var NEW_DOC = {
   'newDocumentDocument': 'http://docs.google.com/MiscCommands?command=newdoc&redirectToDocument=true&hl=en',
   'newDocumentSpreadsheet': 'http://spreadsheets.google.com/ccc?new&hl=en',
   'newDocumentPresentation': 'http://docs.google.com/DocAction?action=new_presentation&source=doclist&hl=en',
-  'newDocumentForm': 'http://spreadsheets.google.com/newform?hl=en'
-};
+  'newDocumentForm': 'http://spreadsheets.google.com/newform?hl=en' };
 
 var LOGIN_ERRORS = {
   'BadAuthentication': ERROR_BAD_AUTH,
@@ -90,15 +86,13 @@ var LOGIN_ERRORS = {
   'AccountDeleted': ERROR_ACCOUNT_DELETED,
   'AccountDisabled': ERROR_ACCOUNT_DISABLED,
   'ServiceDisabled': ERROR_SERVICE_DISABLED,
-  'ServiceUnavailable': ERROR_SERVICE_UNAVAILABLE
-};
+  'ServiceUnavailable': ERROR_SERVICE_UNAVAILABLE };
 
 var UPLOAD_ERRORS = {
   STATUS_415: ERROR_UPLOAD_TYPE,
   STATUS_401: ERROR_UPLOAD_EXPIRED,
   STATUS_0: ERROR_UPLOAD_NETWORK,
-  CORRUPT: ERROR_UPLOAD_CORRUPT
-};
+  CORRUPT: ERROR_UPLOAD_CORRUPT };
 
 var KEYS = {
   ENTER: 13,
@@ -109,5 +103,4 @@ var KEYS = {
   PAGE_UP: 33,
   PAGE_DOWN: 34,
   HOME: 36,
-  END: 35
-};
+  END: 35 };
